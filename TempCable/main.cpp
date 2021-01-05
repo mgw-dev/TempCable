acb2a8a020dd49b1ffc9d9ce86b00a50045b22ec
Mark Wagner <markwagner96@gmail.com>
2021-01-04 22:33:56 -0700

    Add project files.

---
+++ TempCable/main.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <fstream>
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+const std::string DEVICEPATH = "/sys/bus/w1/devices/";
+
+
+float getTemp(std::string s) {
+    float output;
+    std::stringstream ss;
+    ss << DEVICEPATH << s << "/temperature";
+    std::string path = ss.str();
+    std::ifstream file(path);
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            output = atof(line.c_str()) / 1000;
+        }
+        file.close();
+    }
+    return output;
+}
+
+void getSensors(std::vector<std::string>& v) {
+    system("sudo modprobe w1-gpio");
+    system("sudo modprobe w1-therm");
+
+    DIR* dirp = opendir(DEVICEPATH.c_str());
+    struct dirent* dp;
+    while ((dp = readdir(dirp)) != nullptr) {
+        if (strstr(dp->d_name, "28-00000"))
+        {
+            v.push_back(dp->d_name);
+        }
+    }
+    closedir(dirp);
+}
+
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> sensors;
+    getSensors(sensors);
+    printf("\n");
+	while(1)
+	{
+		for (auto sensorID : sensors)
+		{
+            printf(sensorID.c_str());
+            printf("\t");
+            printf("temp: %3.3f °C", getTemp(sensorID));
+            printf("\n");
+		}
+        printf("\n");
+		sleep(5);
+	}
+
+}
